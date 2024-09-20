061f0d9b1ee1b19f78172392b1f2f49125f78d81
orlapez <orlapez@gmail.com>
2024-09-19 21:16:44 -0500

    Veterinary. API - Controller owner- All Methods

---
+++ Veterinary.API/Controllers/OwnerController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Veterinary.API.Data;
+using Veterinary.Shared.Entities;
+
+namespace Veterinary.API.Controllers
+{
+
+
+    [ApiController]
+    [Route("/api/owners")]
+    public class OwnerController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+
+        public OwnerController(DataContext context)
+
+        {
+
+            _context = context;
+
+        }
+
+
+        //get de lista - Select * from owners
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+
+            return Ok(await _context.Owners.ToListAsync());
+
+        }
+
+
+        //Get con parámetro {id}
+        [HttpGet("{id:int}")]
+
+        public async Task<ActionResult> Get(int id)
+        {
+
+            var busqueda = await _context.Owners.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (busqueda == null)
+            {
+
+                return NotFound();
+
+
+
+            }
+            else
+            {
+
+                return Ok(busqueda);
+            }
+
+        }
+        //Crear un nuevo Owner
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Owner owner)
+        {
+            _context.Owners.Add(owner);
+            await _context.SaveChangesAsync();
+            return Ok(owner);
+
+        }
+
+
+        //Actualizar un Owner
+        [HttpPut]
+        public async Task<ActionResult> Put(Owner owner)
+        {
+            _context.Owners.Update(owner);
+            await _context.SaveChangesAsync();
+            return Ok(owner);
+
+        }
+
+
+        // Eliminar un
+        [HttpDelete("{id:int}")]
+
+        public async Task<ActionResult> Delete(int id)
+        {
+
+            var filasafectadas = await _context.Owners
+                .Where(x => x.Id == id)
+                .ExecuteDeleteAsync();
+
+            if (filasafectadas == 0)
+            {
+
+
+
+                return NotFound();
+            }
+            else
+            {
+
+                return NoContent();
+            }
+
+
+
+        }
+
+    }
+
+}
---
+++ VeterinaryShared/Entities/Owner.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veterinary.Shared.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        [Display(Name="Documento de identidad")]
+        [Required]
+        public string Document { get; set; }
+
+        [Display(Name = "Primer nombre")]
+        [Required]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Apellido")]
+        [Required]
+        public string LastName { get; set; }
+
+        [Display(Name = "Teléfono fijo")]
+        [Required]
+        public int FixedPhone { get; set; }
+
+        [Display(Name = "Móvil")]
+        [Required]
+        public string CellPhone { get; set; }
+
+        [Display(Name = "Dirección")]
+        [Required]
+        public string  Address { get; set; }
+
+        public string FullName=>$"{FirstName}  {LastName}";   
+
+
+
+    }
+}
